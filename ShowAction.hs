63eb928158182ab997954e48eec22eb766be0a04
aiya000 <aiya000.develop@gmail.com>
2016-09-29 21:34:04 +0900

    Implement ShowAction.hs for checking

--- ShowAction.hs
+++ ShowAction.hs
@@ -1,4 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Control.SentenceJP
+import qualified Data.Text.IO as TIO
+
+
 main :: IO ()
-main = print "ok"
+main = do
+  sentence <- generateSentence
+    [ "僕は貴方ではない"
+    , "貴方は魔術師だ"
+    , "魔術師は強い"
+    , "汝、強さを求めるか"
+    , "あっ、やっちまった"
+    ]
+  TIO.putStrLn sentence
