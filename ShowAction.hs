fdded92ba59fb4d43f868efd76b05d7c34f46159
aiya000 <aiya000.develop@gmail.com>
2017-01-27 18:15:53 +0900

    Update ShowAction for new implementation

--- ShowAction.hs
+++ ShowAction.hs
@@ -2,7 +2,8 @@
 
 module Main where
 
-import Control.SentenceJP (generateMessage)
+import Control.Monad (mapM_)
+import Control.SentenceJP (generateMessage, GenerateOption (IgnoreSigns, IgnoreAlphaNums))
 import qualified Data.Text as Text
 import qualified Data.Text.IO as TIO
 
@@ -10,7 +11,11 @@ import qualified Data.Text.IO as TIO
 main :: IO ()
 main = do
   texts    <- Text.lines <$> TIO.readFile "sentences.txt"
-  sentence <- generateMessage texts
-  case sentence of
-    Left  e -> error e
-    Right a -> TIO.putStrLn a
+  putStr "plain: "
+  generateMessage [] texts >>= mapM_ TIO.putStrLn
+  putStr "ignore signs: "
+  generateMessage [IgnoreSigns] texts >>= mapM_ TIO.putStrLn
+  putStr "ignore alphanums: "
+  generateMessage [IgnoreAlphaNums] texts >>= mapM_ TIO.putStrLn
+  putStr "ignore signs alphanums: "
+  generateMessage [IgnoreSigns] texts >>= mapM_ TIO.putStrLn
